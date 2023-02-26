5ceae99af824d38693fb74ffaacda692deae4529
xCaio <caiosantos1703@gmail.com>
2023-02-26 20:21:33 -0300

    add api barcode

--- script/script.js
+++ script/script.js
@@ -1,14 +1,20 @@
-nome = window.document.querySelector("#nome");
-idade = window.document.querySelector("#idade");
-nExame = window.document.querySelector("#nExame");
+let nome = window.document.querySelector("#nome");
+let idade = window.document.querySelector("#idade");
+let nExame = window.document.querySelector("#nExame");
+let infoExame = window.document.querySelector("#infoNExame")
 
+let labelNome = window.document.querySelector("#labelNome");
+let labelIdade = window.document.querySelector("#labelIdade");
+let labelNExame = window.document.querySelector("#labelNExame");
 
-labelNome = window.document.querySelector("#labelNome");
-labelIdade = window.document.querySelector("#labelIdade");
-labelNExame = window.document.querySelector("#labelNExame");
 
 function enviar (){
     labelNome.innerHTML = nome.value;
     labelIdade.innerHTML = idade.value;
     labelNExame.innerHTML = nExame.value;
+    var googleChartAPI = `http://bwipjs-api.metafloor.com/?bcid=code128&scale=3&text=${nExame.value}`
+    document.querySelector("#labelNExame").src = googleChartAPI;
+    infoExame.innerHTML = nExame.value;
+    
 }
+
